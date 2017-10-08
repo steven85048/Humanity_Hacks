a604163706913b3e33b40c565fd04e83a539dda1
steven85048 <steven85048@gmail.com>
2017-10-07 17:05:29 -0700

    updated android stuff

--- Humanity_Hacks/app/src/main/java/com/example/steve/humanity_hacks/MainActivity.java
+++ Humanity_Hacks/app/src/main/java/com/example/steve/humanity_hacks/MainActivity.java
@@ -2,14 +2,133 @@ package com.example.steve.humanity_hacks;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.nearby.Nearby;
+import com.google.android.gms.nearby.messages.Message;
+import com.google.android.gms.nearby.messages.MessageListener;
+
+public class MainActivity extends AppCompatActivity implements
+                                                        GoogleApiClient.ConnectionCallbacks,
+                                                        GoogleApiClient.OnConnectionFailedListener{
+
+    // Google API client variables
+    GoogleApiClient mGoogleApiClient;
+    Message activeMessage;
+    MessageListener messageListener;
+
+    // Component variables
+    Button submitButton;
+
+    // ========================== MAIN THREAD THREADS ==============================
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Component set up
+        submitButton = (Button) findViewById(R.id.submitButton);
+        submitButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                click();
+            }
+        });
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(Nearby.CONNECTIONS_API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+
+        Log.e("Testing", "Testing");
+
+        messageListener = new MessageListener() {
+            @Override
+            public void onFound(Message message){
+                String currMessage =  new String(message.getContent());
+                Log.d("Found message: ", currMessage);
+            }
+
+            @Override
+            public void onLost(Message message){
+                String currMessage = new String(message.getContent());
+                Log.d("Lost message: ", currMessage);
+            }
+        };
     }
 
+    @Override
+    public void onStart(){
+        Log.e("Starting", "Starting");
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
 
+    @Override
+    public void onStop() {
+        if (mGoogleApiClient.isConnected()) {
+            mGoogleApiClient.disconnect();
+        }
+
+        super.onStop();
+    }
+
+    // ============================== MESSAGE RECEPTION SERVICE ==========================
+
+    // Subscribe to receive messages.
+    private void subscribe() {
+        Log.i("Subscribing", "Subscribing.");
+        Nearby.Messages.subscribe(mGoogleApiClient,messageListener);
+    }
+
+    // Unsubscribe to receiving messages
+    private void unsubscribe(){
+        Log.i("Unsubscribing", "Unsubscribing");
+        Nearby.Messages.unsubscribe(mGoogleApiClient, messageListener);
+    }
+
+    // ============================== MESSAGE PUBLISHING SERVICE =========================
+
+    private void publish(String message){
+        Log.e("Message", message);
+        activeMessage = new Message(message.getBytes());
+        Nearby.Messages.publish(mGoogleApiClient, activeMessage);
+    }
+
+    private void unpublish(){
+        Log.i("Unpublishing", "Unpublished");
+        if (activeMessage != null){
+            Nearby.Messages.unpublish(mGoogleApiClient, activeMessage);
+            activeMessage = null;
+        }
+    }
+
+    // ===================== CONNECTION CALLBACKS ===========================
+
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        Log.e("fuck", "fuck me in the asshole");
+        // TODO
+    }
+
+    @Override
+    public void onConnectionSuspended(int cause) {
+        // TODO
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        // TODO
+    }
+
+    // ===================== Component Events ========================
+    public void click() {
+        publish("hello");
+    }
 }
